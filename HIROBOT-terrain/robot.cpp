b99502037e7287cca5b927c05829e6caf87bfd01
VeronikaKolo <vero46.kolo@gmail.com>
2020-01-03 11:03:59 +0100

    ajout de commentaire et fonction dans les fichiers des robots

--- HIROBOT-terrain/robot.cpp
+++ HIROBOT-terrain/robot.cpp
@@ -1,17 +1,57 @@
 #include "robot.h"
 
-
+/** Construction d'un robot par defaut c'est a dire que sa position est [0;0] */
 robot::robot():element{nullptr}
 {}
+
+/**Construction du robot a l'aide d'une position */
 robot::robot(position*p):element{p}
 {}
 
+/**Destruction du robot */
 robot::~robot(){}
 
+/**Affiche la position du robot*/
 void robot::affichePosition()const{
 
     std::cout<<positionElement()->numLigne()<<";"<<positionElement()->numColonne()<<std::endl;
 }
 
+/**Verifie s'il y a collision avec le robot
+    @param[in] r-le robot
+    @return si les positions des robots sont les memes
+*/
+
+bool robot:: CollisionRobot(robot&r){
+    return  positionElement()==r.positionElement();
+}
+
+/**Verifie s'il y a collision avec le joueur
+    @param[in] j-le joueur
+    @return si les positions du robot et du joueur sont les memes
+*/
+
+bool robot :: CollisionJoueur(joueur&j){
+    return  positionElement()==j.positionElement();
+}
+
+/**Verifie s'il y a collision avec le debris
+    @param[in] d-le debris
+    @return si les positions du robot et du debris sont les memes
+*/
+
+bool robot ::CollisionDebris(debris&d){
+    return  positionElement()==d.positionElement();
+}
+
+/**Affiche la position du robot
+    @param[in] ost-le flot d'entree
+    @param[in] r-le robot
+    @return le flot
+*/
+std::ostream& operator<<(std::ostream&ost, robot&r){
+    r.sauverPosition(ost);
+    return ost;
+}
 
 
--- HIROBOT-terrain/robot1g.cpp
+++ HIROBOT-terrain/robot1g.cpp
@@ -1,15 +1,24 @@
 #include "robot1g.h"
 #include  <cmath>
+
+/** Construction d'un robot par defaut c'est a dire que sa position est [0;0] */
 robot1G::robot1G():robot{nullptr}
 {}
 
+/**Construction du robot a l'aide d'une position */
 robot1G::robot1G(position*p):robot{p}
 {}
 
+/** Retourne le type du robot
+ @return la valeur du type de robot qui est 0
+ */
 int robot1G::type()const{
     return 0;
 }
 
+/**Deplace le robot en fonction de la position du joueur
+ @param[in] j-le joueur
+*/
 void robot1G::deplacerRobot(const joueur&j){
 
     int distanceColonne,distanceLigne,distanceAbsolueColonne,distanceAbsolueLigne;
